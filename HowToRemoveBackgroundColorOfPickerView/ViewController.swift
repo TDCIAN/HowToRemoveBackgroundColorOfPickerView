92547d5a8fc49e17f9abc9ec9518f9f5e1d4352f
Jeongmin <tdcian71@gmail.com>
2021-03-14 23:21:15 +0900

    잘못된 사례 설명

--- HowToRemoveBackgroundColorOfPickerView/ViewController.swift
+++ HowToRemoveBackgroundColorOfPickerView/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+extension ViewController: UIPickerViewAccessibilityDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
 
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        let numberLabel = UILabel()
+        numberLabel.textAlignment = .center
+        numberLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        numberLabel.textColor = .systemBlue
+        numberLabel.text = "\(row)"
+        
+        return numberLabel
+    }
 }
 
